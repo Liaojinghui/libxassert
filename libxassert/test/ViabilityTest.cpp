130a50179645bf697e6adc449593bfe34c2ec34a
Alin Tomescu <alinush@mit.edu>
2018-06-09 17:25:15 -0700

    missing assertIsPositive

--- libxassert/test/ViabilityTest.cpp
+++ libxassert/test/ViabilityTest.cpp
@@ -8,38 +8,40 @@ int main(int argc, char * argv[]) {
 
     // NOTE: Implicitly tests XAssert's static initializer!
 
-    assertTrue(true);
-    assertFalse(false);
+    testAssertTrue(true);
+    testAssertFalse(false);
 
-    assertInclusiveRange(0, 0, 0);
-    assertInclusiveRange(0, 0, 1);
-    assertInclusiveRange(-1, 0, 0);
+    testAssertInclusiveRange(0, 0, 0);
+    testAssertInclusiveRange(0, 0, 1);
+    testAssertInclusiveRange(-1, 0, 0);
 
-    assertStrictlyGreaterThan(1, 0);
-    assertStrictlyLessThan(0, 1);
+    testAssertStrictlyGreaterThan(1, 0);
+    testAssertStrictlyLessThan(0, 1);
 
-    assertGreaterThanOrEqual(1, 1);
-    assertGreaterThanOrEqual(2, 1);
+    testAssertGreaterThanOrEqual(1, 1);
+    testAssertGreaterThanOrEqual(2, 1);
 
-    assertLessThanOrEqual(0, 0);
-    assertLessThanOrEqual(0, 1);
+    testAssertLessThanOrEqual(0, 0);
+    testAssertLessThanOrEqual(0, 1);
 
-    assertEqual(0, 0);
-    assertIsZero(0);
-    assertNotZero(1);
+    testAssertEqual(0, 0);
+    testAssertIsZero(0);
+    testAssertNotZero(1);
 
     // This one is a hard one to test!
     //assertFail(shouldNotBeCalled());
 
     int val = 3;
-    assertProperty(val, val % 2 == 1 && val > 2);
+    testAssertProperty(val, val % 2 == 1 && val > 2);
 
-    assertNotNull(std::unique_ptr<int>(new int(3)).get());
-    assertNotEqual(1, 0);
+    testAssertNotNull(std::unique_ptr<int>(new int(3)).get());
+    testAssertNotEqual(1, 0);
 
-    assertStrictlyPositive(1);
+    testAssertIsPositive(0);
+    testAssertIsPositive(1);
+    testAssertStrictlyPositive(1);
 
-    assertNull(nullptr);
+    testAssertNull(nullptr);
 
     std::cout << "Test '" << argv[0] << "' passed!" << std::endl;
 
